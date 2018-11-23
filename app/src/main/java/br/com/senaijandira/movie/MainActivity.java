b403762abc7c58477950b5a14e980f7b32f82159
miacelPereira <micael.pereira1999@hotmail.com>
2018-11-22 22:56:57 -0200

    Feita uma Splash sreen e finalizada a barra de pesquisa

--- app/src/main/java/br/com/senaijandira/movie/MainActivity.java
+++ app/src/main/java/br/com/senaijandira/movie/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText txtBusca;
     ImageView imgLogo;
+    int cont;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +19,19 @@ public class MainActivity extends AppCompatActivity {
 
         txtBusca = findViewById(R.id.txtBusca);
         imgLogo = findViewById(R.id.imgLogo);
+        cont = 0;
     }
 
     public void abrirEdittext(View view) {
-        int cont = 0;
-        /* Esquema para o logo sumir */
+
+        // Estrutura de decisão para mostrar o Edit text
         if(cont == 0){
-            txtBusca.setMinimumWidth(250);
-            imgLogo.setMaxWidth(0);
-
-
+            imgLogo.setVisibility(view.GONE);
+            txtBusca.setVisibility(view.VISIBLE);
+            cont++;
         }else{
-
+            imgLogo.setVisibility(view.VISIBLE);
+            txtBusca.setVisibility(view.GONE);
             cont = 0;
         }
     }
---
+++ app/src/main/java/br/com/senaijandira/movie/SplashActivity.java
@@ -0,0 +1,29 @@
+package br.com.senaijandira.movie;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        }, 5000);
+
+
+    }
+}
